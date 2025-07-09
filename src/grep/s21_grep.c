607e7b281ec178b95cdda106a25209580d689469
keka <kvtka4enko@yandex.ru>
2025-07-09 16:04:14 +0300

    feat: grep init commit

---
+++ src/grep/s21_grep.c
@@ -0,0 +1,11 @@
+#include "s21_grep.h"
+#include <stdio.h>
+#include <getopt.h>
+
+int main(int argc, char *argv[]) {
+    grep_flags flags = {0, 0, 0, 0, 0, 0, 0};
+
+
+    
+    return 0;
+}
---
+++ src/grep/s21_grep.h
@@ -0,0 +1,8 @@
+#ifndef S21_GREP_H
+#define S21_GREP_H
+
+typedef struct {
+  int e, i, v, c, l, n, error;
+} grep_flags;
+
+#endif
